11ea18d358e3d865b013f92e9747a88824e3a5fc
Bartegu <bartegutv@gmail.com>
2025-02-01 15:32:30 +0100

    Update EldoGaming.py to include sample data and transform columns into rows for improved data presentation

--- EldoGaming.py
+++ EldoGaming.py
@@ -3,17 +3,25 @@ import pandas as pd
 st.set_page_config(layout="wide")
 st.title("Wyjscia melanze")
 
-df = pd.DataFrame({"MIEJSCE": [], "OCENIAJĄCY": [],
-                   "FOOD":[],
-                   "WYSTRÓJ":[],
-                   "OBSŁUGA":[],
-                   "PERFORMANCE PER PRICE":[],
-                   "INNE":[],
-                   "ŚREDNIA Z PUNKTÓW":[],
-                   "ŚREDNIA Z MIEJSCÓWKI":[],
-                   "KOMENTARZ":[]})
+# Przykładowe dane
+df = pd.DataFrame({
+    "MIEJSCE": ["Restauracja A", "Restauracja B"],
+    "OCENIAJĄCY": ["Jan", "Anna"],
+    "FOOD": [5, 4],
+    "WYSTRÓJ": [4, 3],
+    "OBSŁUGA": [5, 4],
+    "PERFORMANCE PER PRICE": [3, 4],
+    "INNE": [4, 5],
+    "ŚREDNIA Z PUNKTÓW": [4.2, 4.0],
+    "ŚREDNIA Z MIEJSCÓWKI": [4.1, 3.9],
+    "KOMENTARZ": ["Bardzo dobre", "Średnie"]
+})
 
-st.data_editor(data=df, key="data_editor",num_rows="dynamic")
+# Zamiana kolumn na wiersze
+df_melted = df.melt(var_name="KATEGORIA", value_name="WARTOŚĆ")
 
-if st.button("Click me"):
-    st.success("You clicked me!")
+# Dodanie numeracji
+df_melted.insert(0, "LP", range(1, len(df_melted) + 1))
+
+
+st.dataframe(df_melted,hide_index=True)
