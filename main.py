382bc15cf06f5dbaafebd923604bcf02dfc61e7d
Bartegu <bartegutv@gmail.com>
2025-02-01 10:55:03 +0100

    Add navigation to a second page in main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title("Hello world!")
 st.code("Coding in progress...")
+
+
+def page_2():
+    st.title("Page 2")
+
+pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
+pg.run()
