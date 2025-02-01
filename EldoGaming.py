549806da90f48607acf5760162b3544df9b56c40
Bartegu <bartegutv@gmail.com>
2025-02-01 15:00:31 +0100

    Add custom CSS to hide the GitHub icon in EldoGaming.py

--- EldoGaming.py
+++ EldoGaming.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.title("Wyjscia melanze")
 
 df = pd.DataFrame({"MIEJSCE": [], "OCENIAJĄCY": []})
