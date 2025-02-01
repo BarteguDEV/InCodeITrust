d15a86b54a91ad4afbf649b3382ae5307b540423
Bartegu <bartegutv@gmail.com>
2025-02-01 15:22:01 +0100

    Refactor EldoGaming.py and manage_account.py for layout adjustments and enhanced data structure

--- EldoGaming.py
+++ EldoGaming.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
+st.set_page_config(layout="wide")
 st.title("Wyjscia melanze")
 
-df = pd.DataFrame({"MIEJSCE": [], "OCENIAJĄCY": []})
+df = pd.DataFrame({"MIEJSCE": [], "OCENIAJĄCY": [],
+                   "FOOD":[],
+                   "WYSTRÓJ":[],
+                   "OBSŁUGA":[],
+                   "PERFORMANCE PER PRICE":[],
+                   "INNE":[],
+                   "ŚREDNIA Z PUNKTÓW":[],
+                   "ŚREDNIA Z MIEJSCÓWKI":[],
+                   "KOMENTARZ":[]})
 
-st.data_editor(data=df, key="data_editor")
+st.data_editor(data=df, key="data_editor",num_rows="dynamic")
 
 if st.button("Click me"):
     st.success("You clicked me!")
---
+++ manage_account.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(layout="centered")
+st.title("Wyjscia melanze")
