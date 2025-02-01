d197b0eb48b297f309402b907590d2fd6c294052
Bartegu <bartegutv@gmail.com>
2025-02-01 15:42:39 +0100

    Add selection box for location and display updated dataframe in EldoGaming.py

--- EldoGaming.py
+++ EldoGaming.py
@@ -3,6 +3,10 @@ import pandas as pd
 st.set_page_config(layout="wide")
 st.title("Wyjscia melanze")
 
+option = st.selectbox(
+    "Wybierz miejscowke",
+    ("SKUNSTREFA", "PIZZA", "KFC"),
+)
 # Przykładowe dane
 df = pd.DataFrame({
     "MIEJSCE": ["Restauracja A", "Restauracja B"],
@@ -23,5 +27,7 @@ df_melted = df.melt(var_name="KATEGORIA", value_name="WARTOŚĆ")
 # Dodanie numeracji
 df_melted.insert(0, "LP", range(1, len(df_melted) + 1))
 
+df_melted["MIEJSCE"] = option
+st.dataframe(df_melted,hide_index=True, width=500, height=500)
 
-st.dataframe(df_melted,hide_index=True)
+st.bar_chart(df["INNE"])
