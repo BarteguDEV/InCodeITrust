c1e08e2a05cf08d5b37d78836e3ed15ee0b0d422
Bartegu <bartegutv@gmail.com>
2025-02-01 14:57:51 +0100

    Remove CSS styles to hide specific elements in main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.logo("https://www.streamlit.io/images/brand/streamlit-mark-color.png")
 pages = {
     "Home": [
