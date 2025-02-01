1eec7d4b07a498c483dc3ff11857fbd84c4fa37a
Bartegu <bartegutv@gmail.com>
2025-02-01 14:03:22 +0100

    Add multi-page navigation and initial page files

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import streamlit as st
 st.title("Hello world!")
 st.code("Coding in progress...")
 
+st.logo("https://www.streamlit.io/images/brand/streamlit-mark-color.png")
+pages = {
+    "Home": [
+        st.Page("Welcome.py", title="Welcome",icon="🏠"),
+    ],
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ]
+}
 
-def page_2():
-    st.title("Page 2")
-
-pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
+pg = st.navigation(pages)
 pg.run()
