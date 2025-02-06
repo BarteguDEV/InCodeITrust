72c7675bf9deb78323b4036bd90e5fd1c5e50c54
Bartegu <bartegutv@gmail.com>
2025-02-06 20:17:45 +0100

    Add admin login functionality and conditional database access in main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import streamlit as st
 st.set_page_config(page_title="EldoGaming", page_icon="🎮", layout="wide")
 
-st.logo("https://www.streamlit.io/images/brand/streamlit-mark-color.png")
+# Strona boczna do logowania
+if "admin_logged_in" not in st.session_state:
+    st.session_state.admin_logged_in = False
+
+# Wprowadzenie hasła w sidebarze
+if not st.session_state.admin_logged_in:
+    with st.sidebar:
+        admin_pass = st.text_input("Admin password", type="password")
+        
+        if admin_pass == st.secrets["barteguapp"]["secret_pass"]:
+            st.session_state.admin_logged_in = True
+            st.success("Hasło poprawne. Możesz uzyskać dostęp do zakładki 'Database'.")
+
+# Definicja stron
 pages = {
     "Home": [
         st.Page("Welcome.py", title="Welcome", icon="🏠")
     ],
     "Apps": [
         st.Page("EldoGaming.py", title="EldoGaming", icon="🎮")
-    ],
-    "Database": [
-        st.Page("Supabase.py", title="Supabase", icon="🔗"),
     ]
 }
 
+# Jeśli hasło jest poprawne, dodajemy zakładkę "Database"
+if st.session_state.admin_logged_in:
+    pages["Database"] = [
+        st.Page("Supabase.py", title="Supabase", icon="🔗")
+    ]
+
+# Nawigacja
 pg = st.navigation(pages)
 pg.run()
