acd31042ba5ed0d1ca5cf2b2dc5d8fcf2755b02a
Bartegu <bartegutv@gmail.com>
2025-02-06 19:49:46 +0100

    Adjust venue image display width and remove duplicate name from persons list in configuration

--- EldoGaming.py
+++ EldoGaming.py
@@ -47,7 +47,7 @@ def display_venue_image(selected_venue, bucket, venues: list):
         
         if file_exists:
             image_url = bucket.get_public_url(file_path)
-            st.image(image_url, width=500)
+            st.image(image_url, width=350)
         else:
             st.info("Brak zdjęcia dla wybranej miejscówki.")
 
